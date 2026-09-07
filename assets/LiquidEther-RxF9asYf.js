51d727ab8d4314b9dfdd4c3d610a84e1580ba53e
Amanyous <airtonlauber766@gmail.com>
2026-09-07 13:18:41 +0800

    Updates

--- assets/LiquidEther-RxF9asYf.js
+++ assets/LiquidEther-RxF9asYf.js
@@ -1,4 +1,4 @@
-import{r as Gt,j as Po}from"./index-O8M1cpIn.js";/**
+import{r as Gt,j as Po}from"./index-D5Wk0qNR.js";/**
  * @license
  * Copyright 2010-2025 Three.js Authors
  * SPDX-License-Identifier: MIT
