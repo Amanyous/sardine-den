47488463c958d9b3dd33649686b4f5e300d7c4b8
其名为沙丁鱼的猫 <airtonlauber766@gmail.com>
2026-09-11 00:08:22 +0800

    deploy artist name update

--- assets/LiquidEther-CDmdKISx.js
+++ assets/LiquidEther-CDmdKISx.js
@@ -1,4 +1,4 @@
-import{r as It,j as Po}from"./index-Djg99k2i.js";/**
+import{r as It,j as Po}from"./index-p-z8gq4e.js";/**
  * @license
  * Copyright 2010-2025 Three.js Authors
  * SPDX-License-Identifier: MIT
