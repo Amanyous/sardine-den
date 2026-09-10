45b6367df78c64f3924246f48da00e1b1020ce8d
Amanyous <airtonlauber766@gmail.com>
2026-09-10 23:04:24 +0800

    deploy runtime optimizations and 1.6.0 changelog

--- assets/LiquidEther-BHHEipmP.js
+++ assets/LiquidEther-BHHEipmP.js
@@ -1,4 +1,4 @@
-import{r as It,j as Po}from"./index-D2zjGmnL.js";/**
+import{r as It,j as Po}from"./index-6sfd3Akc.js";/**
  * @license
  * Copyright 2010-2025 Three.js Authors
  * SPDX-License-Identifier: MIT
